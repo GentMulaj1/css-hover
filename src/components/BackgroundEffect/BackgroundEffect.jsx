c463a53775c9834ff12cc551d7e5b202f074731f
GentMulaj1 <mulajgent2001@gmail.com>
2023-11-13 20:24:08 +0100

    fix styled

--- src/components/BackgroundEffect/BackgroundEffect.jsx
+++ src/components/BackgroundEffect/BackgroundEffect.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './BackgroundEffect.css'
-import styled from 'styled-components';
 
 const BackgroundEffect = ({ text, fullname, firstColor, secondColor}) => {
   return (
